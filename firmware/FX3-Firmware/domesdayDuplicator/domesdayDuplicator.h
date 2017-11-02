45542dd3f966e27ec6c69b9b354055e4e772f20b
Simon Inns <simon.inns@gmail.com>
2017-11-02 16:08:52 +0100

    Initial commit of Cypress FX3 firmware
    Initial commit of Cypress FX3 firmware

---
+++ firmware/FX3-Firmware/domesdayDuplicator/domesdayDuplicator.h
@@ -0,0 +1,90 @@
+/************************************************************************
+
+	domesdayDuplicator.h
+
+	FX3 Firmware main functions
+	DomesdayDuplicator - LaserDisc RF sampler
+	Copyright (C) 2017 Simon Inns
+
+	This file is part of Domesday Duplicator.
+
+	Domesday Duplicator is free software: you can redistribute it and/or
+	modify it under the terms of the GNU General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	Email: simon.inns@gmail.com
+
+************************************************************************/
+
+#ifndef _DOMESDAYDUPLICATOR_H_
+#define _DOMESDAYDUPLICATOR_H_
+
+#include "cyu3usb.h"
+#include "cyu3types.h"
+#include "cyu3usbconst.h"
+#include "cyu3externcstart.h"
+#include "cyu3gpif.h"
+
+#define CY_FX_GPIFTOUSB_DMA_TX_SIZE        (0) 		// DMA transfer size is set to infinite
+#define CY_FX_GPIFTOUSB_THREAD_STACK       (0x1000) // Application thread stack size
+#define CY_FX_GPIFTOUSB_THREAD_PRIORITY    (8) 		// Application thread priority
+#define CY_FX_GPIFTOUSB_PATTERN            (0xAA) 	// 8-bit pattern to be loaded into the source buffers
+
+// Define Event Flag values
+// First 22 Events are defined in cyu3usb.h at "typedef enum CyU3PUsbEventType_t"
+#define USB_EVENTS				(0x7FFFFF)
+#define USER_COMMAND_AVAILABLE	(23)
+#define ANY_EVENT				(USB_EVENTS + (1<<USER_COMMAND_AVAILABLE))
+
+// End-point and socket definitions
+#define CY_FX_EP_CONSUMER               0x81
+#define CY_FX_EP_CONSUMER_SOCKET        CY_U3P_UIB_SOCKET_CONS_1
+#define CY_FX_GPIF_PRODUCER_SOCKET0     CY_U3P_PIB_SOCKET_0
+
+// Set USB 3 burst length to 16K
+#define CY_FX_EP_BURST_LENGTH           (16)
+
+// Set the DMA buffer size to 16K for the application
+#define CY_FX_DMA_BUF_SIZE              (16384)
+
+// Set the total number of DMA buffers available to 4
+#define CY_FX_DMA_BUF_COUNT             (4)
+
+// Extern definitions for the USB Descriptors
+extern const uint8_t USB20DeviceDscr[];
+extern const uint8_t USB30DeviceDscr[];
+extern const uint8_t USBDeviceQualDscr[];
+extern const uint8_t USBFSConfigDscr[];
+extern const uint8_t USBHSConfigDscr[];
+extern const uint8_t USBBOSDscr[];
+extern const uint8_t USBSSConfigDscr[];
+extern const uint8_t USBStringLangIDDscr[];
+extern const uint8_t USBManufactureDscr[];
+extern const uint8_t USBProductDscr[];
+extern const uint8_t USBOSDscr[];
+
+#include <cyu3externcend.h>
+
+// Function prototypes
+void domDupThreadInitialise(uint32_t input);
+void CyFxApplicationDefine(void);
+void domDupInitialiseApplication(void);
+void domDupStartApplication(void);
+void domDupStopApplication(void);
+void domDupErrorHandler(CyU3PReturnStatus_t apiReturnStatus);
+void domDupDebugInit(void);
+
+void GpifDmaEventCB(CyU3PGpifEventType Event, uint8_t State);
+CyBool_t domDupUSBSetupCB(uint32_t setupData0, uint32_t setupData1);
+void domDupUSBEventCB(CyU3PUsbEventType_t eventType, uint16_t eventData);
+CyBool_t domDupLPMRequestCB(CyU3PUsbLinkPowerMode linkMode);
+
+#endif // _DOMESDAYDUPLICATOR_H_
