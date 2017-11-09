7f1ba9eff2914c72bcd4fade1c303fbc90722460
Simon Inns <simon.inns@gmail.com>
2017-11-09 08:37:12 +0100

    Modified build environment for FX3
    Changes to allow FX3 development to use the github directories as workspace and include the build results in the repository

---
+++ firmware/FX3-Firmware/domesdayDuplicator/Debug/cyfxtx.d
@@ -0,0 +1,25 @@
+cyfxtx.d: ../cyfxtx.c \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3os.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3types.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3externcstart.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3tx.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/tx_api.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/tx_port.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3externcend.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3error.h
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3os.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3types.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3externcstart.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3tx.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/tx_api.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/tx_port.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3externcend.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3error.h:
---
+++ firmware/FX3-Firmware/domesdayDuplicator/Debug/domesdayDuplicator.d
@@ -0,0 +1,85 @@
+domesdayDuplicator.d: ../domesdayDuplicator.c \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3system.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3types.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyfx3_api.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3externcstart.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3externcend.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3dma.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3os.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3types.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3tx.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/tx_api.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/tx_port.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3os.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3error.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3usb.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3usbconst.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3uart.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3lpp.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3gpio.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3system.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3utils.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3pib.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3gpif.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3dma.h \
+ ../domesdayDuplicator.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3usbconst.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3externcstart.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3externcend.h \
+ ../domesdayDuplicatorGpif.h
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3system.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3types.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyfx3_api.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3externcstart.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3externcend.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3dma.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3os.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3types.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3tx.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/tx_api.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/tx_port.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3os.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3error.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3usb.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3usbconst.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3uart.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3lpp.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3gpio.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3system.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3utils.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3pib.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3gpif.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3dma.h:
+
+../domesdayDuplicator.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3usbconst.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3externcstart.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\\firmware\u3p_firmware\inc/cyu3externcend.h:
+
+../domesdayDuplicatorGpif.h:
