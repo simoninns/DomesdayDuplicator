e14fdd8fd0961320177cd779fd1ed53c20165812
simoninns <simon.inns@gmail.com>
2017-11-22 18:44:58 +0100

    Added missing GPL headers

--- gui-app/Linux-Application/DomesdayDuplicator/mainwindow.h
+++ gui-app/Linux-Application/DomesdayDuplicator/mainwindow.h
@@ -1,3 +1,31 @@
+/************************************************************************
+
+    mainwindow.h
+
+    QT GUI Capture application for Domesday Duplicator
+    DomesdayDuplicator - LaserDisc RF sampler
+    Copyright (C) 2017 Simon Inns
+
+    This file is part of Domesday Duplicator.
+
+    Domesday Duplicator is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Email: simon.inns@gmail.com
+
+************************************************************************/
+
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
