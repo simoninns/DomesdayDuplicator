e4c25cb889afac819ca0bf5ee82301d6b3caa78d
simoninns <simon.inns@gmail.com>
2017-12-29 22:55:16 +0100

    Fixed odd verilog bug that was causing corrupt samples

--- DE0-NANO/DomesdayDuplicator/dataGenerator.v
+++ DE0-NANO/DomesdayDuplicator/dataGenerator.v
@@ -96,11 +96,7 @@ always @ (negedge adcClk, negedge nReset) begin
 			end else begin
 				// We are in normal mode, use the ADC data bus
 				// Note: Here we correct the DC offset (see project notes for details)
-				if (adcData <= 10'd65) begin
-					adcDataRead = 0;
-				end else begin
-					adcDataRead = adcData - 10'd65;
-				end
+				adcDataRead = adcData - 10'd65;;
 			end
 		end
 	end
