3d01de3550fbf26542c4f3ca7e28594f4c9aeb4c
simoninns <simon.inns@gmail.com>
2018-08-22 20:56:45 +0200

    Initial commit of LaserDiscAnalyser GUI application

---
+++ Linux-Application/LaserDiscAnalyser/mainwindow.cpp
@@ -0,0 +1,172 @@
+/************************************************************************
+
+    mainwindow.cpp
+
+    RF Sample analyser for Domesday Duplicator
+    DomesdayDuplicator - LaserDisc RF sampler
+    Copyright (C) 2018 Simon Inns
+
+    This file is part of Domesday Duplicator.
+
+    Domesday Duplicator is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Email: simon.inns@gmail.com
+
+************************************************************************/
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    // Set up the UI
+    ui->setupUi(this);
+
+    // Create the UI dialogues
+    aboutDialogue = new About(this);
+
+    // Create the RF sample object
+    rfSample = new RfSample(this);
+
+    // Perform no file loaded actions
+    noFileLoaded();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+// GUI Update functions -----------------------------------------------------------------------------------------------
+
+// Actions on file not loaded
+void MainWindow::noFileLoaded(void)
+{
+    // Menu options
+    ui->actionOpen_File->setEnabled(true);
+    ui->actionSave_As->setEnabled(false);
+
+    // Input file options
+    ui->filenameLineEdit->setText(tr("No file loaded"));
+    ui->numberOfSamplesLabel->setText(tr("0"));
+    ui->sizeOnDiscLabel->setText(tr("0"));
+    ui->durationLabel->setText(tr("00:00:00"));
+
+    // Output file options
+    ui->tenBitRadioButton->setEnabled(false);
+    ui->sixteenBitRadioButton->setEnabled(false);
+    ui->startTimeEdit->setEnabled(false);
+    ui->endTimeEdit->setEnabled(false);
+}
+
+// Actions on file loaded
+void MainWindow::fileLoaded(void)
+{
+    // Menu options
+    ui->actionOpen_File->setEnabled(true);
+    ui->actionSave_As->setEnabled(true);
+
+    // Input file options
+    ui->filenameLineEdit->setText(inputFilename);
+    ui->numberOfSamplesLabel->setText(QString::number(rfSample->getNumberOfSamples()));
+    ui->sizeOnDiscLabel->setText(rfSample->getSizeOnDisc());
+    ui->durationLabel->setText(rfSample->getDurationString());
+
+    // Output file options
+    ui->tenBitRadioButton->setEnabled(true);
+    ui->sixteenBitRadioButton->setEnabled(true);
+    ui->startTimeEdit->setEnabled(true);
+    ui->endTimeEdit->setEnabled(true);
+
+    // Set the initial time-span for the sample
+    ui->startTimeEdit->setTime(QTime(0,0));
+    ui->endTimeEdit->setTime(QTime(0,0).addSecs(static_cast<qint32>(rfSample->getNumberOfSamples() / 40000000)));
+}
+
+// Menu bar action functions ------------------------------------------------------------------------------------------
+
+// Menu bar - Open File triggered
+void MainWindow::on_actionOpen_File_triggered()
+{
+    inputFilename = QFileDialog::getOpenFileName(this,
+            tr("Open LaserDisc RF Sample"), "",
+            tr("LaserDisc Sample (*.lds);;All Files (*)"));
+
+    // Was a filename specified?
+    if (!inputFilename.isEmpty()) {
+        // Attempt to load the file
+        if (rfSample->openInputSample(inputFilename)) {
+            // Update the GUI (success)
+            fileLoaded();
+        } else {
+            // Update the GUI (failure)
+            noFileLoaded();
+        }
+    }
+}
+
+// Menu bar - Save As triggered
+void MainWindow::on_actionSave_As_triggered()
+{
+    outputFilename = QFileDialog::getSaveFileName(this,
+            tr("Save LaserDisc RF Sample"), "",
+            tr("LaserDisc Sample .lds (*.lds);;All Files (*)"));
+
+    // Was a filename specified?
+    if (!outputFilename.isEmpty()) {
+        // Get the file format (10/16-bit)
+        bool isTenBit = true;
+        if (ui->sixteenBitRadioButton->isChecked()) isTenBit = false;
+
+        // Attempt to save the file
+        rfSample->saveOutputSample(outputFilename, ui->startTimeEdit->time(), ui->endTimeEdit->time(), isTenBit);
+    }
+}
+
+// Menu bar - Quit triggered
+void MainWindow::on_actionQuit_triggered()
+{
+    // Close the RF sample if open
+    rfSample->closeInputSample();
+
+    // Quit the application
+    qApp->quit();
+}
+
+// Menu bar - About triggered
+void MainWindow::on_actionAbout_triggered()
+{
+    // Show the about dialogue
+    aboutDialogue->show();
+}
+
+// User changed the sample start time
+void MainWindow::on_startTimeEdit_userTimeChanged(const QTime &startTime)
+{
+    // Ensure that startTime is never greater than end time
+    if (startTime.operator>(ui->endTimeEdit->time())) ui->startTimeEdit->setTime(ui->endTimeEdit->time());
+}
+
+// User changed the sample end time
+void MainWindow::on_endTimeEdit_userTimeChanged(const QTime &endTime)
+{
+    // Ensure that endTime is never less than start time
+    if (endTime.operator<(ui->startTimeEdit->time())) ui->endTimeEdit->setTime(ui->startTimeEdit->time());
+
+    // Ensure that endTime is never greater than the sample length
+    if (endTime.operator>(QTime(0,0).addSecs(rfSample->getDurationSeconds())))
+        ui->endTimeEdit->setTime(QTime(0,0).addSecs(rfSample->getDurationSeconds()));
+}
---
+++ Linux-Application/LaserDiscAnalyser/mainwindow.h
@@ -0,0 +1,70 @@
+/************************************************************************
+
+    mainwindow.h
+
+    RF Sample analyser for Domesday Duplicator
+    DomesdayDuplicator - LaserDisc RF sampler
+    Copyright (C) 2018 Simon Inns
+
+    This file is part of Domesday Duplicator.
+
+    Domesday Duplicator is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Email: simon.inns@gmail.com
+
+************************************************************************/
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFileDialog>
+
+#include "about.h"
+#include "rfsample.h"
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_actionOpen_File_triggered();
+    void on_actionSave_As_triggered();
+    void on_actionQuit_triggered();
+    void on_actionAbout_triggered();
+
+    void on_startTimeEdit_userTimeChanged(const QTime &startTime);
+    void on_endTimeEdit_userTimeChanged(const QTime &endTime);
+
+private:
+    Ui::MainWindow *ui;
+
+    void noFileLoaded(void);
+    void fileLoaded(void);
+
+    QString inputFilename;
+    QString outputFilename;
+    About *aboutDialogue;
+    RfSample *rfSample;
+};
+
+#endif // MAINWINDOW_H
