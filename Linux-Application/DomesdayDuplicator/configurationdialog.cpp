3ae7e4850c10abee1d34a8c345cccbdb1c53c9ff
simoninns <simon.inns@gmail.com>
2018-09-20 10:12:34 +0200

    Added player integration preferences functionality

--- Linux-Application/DomesdayDuplicator/configurationdialog.cpp
+++ Linux-Application/DomesdayDuplicator/configurationdialog.cpp
@@ -61,7 +61,75 @@ void ConfigurationDialog::loadConfiguration(Configuration *configuration)
     ui->productIdLineEdit->setText(QString::number(configuration->getUsbPid()));
 
     // Player Integration
-    // To-do
+
+    // Build the serialDeviceComboBox
+    ui->serialDeviceComboBox->clear();
+    const auto infos = QSerialPortInfo::availablePorts();
+
+    // Add additional "None" option to allow de-selection of COM port
+    ui->serialDeviceComboBox->addItem(QString(tr("None")), QString(tr("None")));
+
+    bool configuredSerialDevicePresent = false;
+    for (const QSerialPortInfo &info : infos) {
+        QStringList list;
+        list << info.portName();
+        ui->serialDeviceComboBox->addItem(list.first(), list);
+
+        // Is this the currently configured serial device?
+        if (info.portName() == configuration->getSerialDevice())
+                configuredSerialDevicePresent = true;
+    }
+
+    // Select the currently configured device (or default to 'none' if the device is not set)
+    if (!configuredSerialDevicePresent) {
+        // No device is present in the configuration or the configured device is no longer available - set to none
+        ui->serialDeviceComboBox->setCurrentIndex(0);
+    } else {
+        // Set to the configured device
+        ui->serialDeviceComboBox->setCurrentIndex(ui->serialDeviceComboBox->findData(configuration->getSerialDevice()));
+    }
+
+    // Build the serialSpeedComboBox
+    ui->serialSpeedComboBox->clear();
+    ui->serialSpeedComboBox->addItem("9600", Configuration::SerialSpeeds::bps9600);
+    ui->serialSpeedComboBox->addItem("4800", Configuration::SerialSpeeds::bps4800);
+    ui->serialSpeedComboBox->addItem("2400", Configuration::SerialSpeeds::bps2400);
+    ui->serialSpeedComboBox->addItem("1200", Configuration::SerialSpeeds::bps1200);
+
+    // Select the currently configured serial speed
+    switch (configuration->getSerialSpeed()) {
+        case Configuration::SerialSpeeds::bps9600:
+        ui->serialSpeedComboBox->setCurrentIndex(0);
+        break;
+        case Configuration::SerialSpeeds::bps4800:
+        ui->serialSpeedComboBox->setCurrentIndex(1);
+        break;
+        case Configuration::SerialSpeeds::bps2400:
+        ui->serialSpeedComboBox->setCurrentIndex(2);
+        break;
+        case Configuration::SerialSpeeds::bps1200:
+        ui->serialSpeedComboBox->setCurrentIndex(3);
+        break;
+    }
+
+    // Build the playerModelComboBox
+    ui->playerModelComboBox->clear();
+    ui->playerModelComboBox->addItem("None", Configuration::PlayerModels::none);
+    ui->playerModelComboBox->addItem("Pioneer LD-V4300D", Configuration::PlayerModels::pioneerLDV4300D);
+    ui->playerModelComboBox->addItem("Pioneer CLD-V2800", Configuration::PlayerModels::pioneerCLDV2800);
+
+    // Select the currently configured player
+    switch(configuration->getPlayerModel()) {
+        case Configuration::PlayerModels::none:
+        ui->playerModelComboBox->setCurrentIndex(0);
+        break;
+        case Configuration::PlayerModels::pioneerLDV4300D:
+        ui->playerModelComboBox->setCurrentIndex(1);
+        break;
+        case Configuration::PlayerModels::pioneerCLDV2800:
+        ui->playerModelComboBox->setCurrentIndex(2);
+        break;
+    }
 }
 
 // Save the configuration settings from the UI widgets
@@ -79,8 +147,30 @@ void ConfigurationDialog::saveConfiguration(Configuration *configuration)
     configuration->setUsbVid(static_cast<quint16>(ui->vendorIdLineEdit->text().toInt()));
     configuration->setUsbPid(static_cast<quint16>(ui->productIdLineEdit->text().toInt()));
 
-    // Player integration
-    // To-do
+    // Player integration - serial device
+    configuration->setSerialDevice(ui->serialDeviceComboBox->currentText());
+
+    // Player integration - Serial speed
+    switch (ui->serialSpeedComboBox->currentIndex()) {
+        case 0: configuration->setSerialSpeed(Configuration::SerialSpeeds::bps9600);
+        break;
+        case 1: configuration->setSerialSpeed(Configuration::SerialSpeeds::bps4800);
+        break;
+        case 2: configuration->setSerialSpeed(Configuration::SerialSpeeds::bps2400);
+        break;
+        case 3: configuration->setSerialSpeed(Configuration::SerialSpeeds::bps1200);
+        break;
+    }
+
+    // Player integration - player model
+    switch (ui->playerModelComboBox->currentIndex()) {
+        case 0: configuration->setPlayerModel(Configuration::PlayerModels::none);
+        break;
+        case 1: configuration->setPlayerModel(Configuration::PlayerModels::pioneerLDV4300D);
+        break;
+        case 2: configuration->setPlayerModel(Configuration::PlayerModels::pioneerCLDV2800);
+        break;
+    }
 
     // Save the configuration to disk
     configuration->writeConfiguration();
@@ -113,4 +203,25 @@ void ConfigurationDialog::on_buttonBox_accepted()
 void ConfigurationDialog::on_buttonBox_rejected()
 {
     qDebug() << "ConfigurationDialog::on_buttonBox_rejected(): Ignoring configuration changes";
+}
+
+// Any button clicked
+void ConfigurationDialog::on_buttonBox_clicked(QAbstractButton *button)
+{
+    // Check for restore defaults button
+    if (button == ui->buttonBox->button(QDialogButtonBox::RestoreDefaults)) {
+        qDebug() << "ConfigurationDialog::on_buttonBox_clicked(): Restore defaults clicked";
+
+        // Set default values for dialog
+        ui->captureDirectoryLineEdit->setText(QDir::homePath());
+        ui->saveAsTenBitRadioButton->setChecked(true);
+        ui->saveAsSixteenBitRadioButton->setChecked(false);
+
+        ui->vendorIdLineEdit->setText(QString::number(7504));
+        ui->productIdLineEdit->setText(QString::number(24635));
+
+        ui->serialDeviceComboBox->setCurrentIndex(0);
+        ui->serialSpeedComboBox->setCurrentIndex(0);
+        ui->playerModelComboBox->setCurrentIndex(0);
+    }
 }
