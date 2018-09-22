0f462ad23186a065ab2045c4e5deb7455cc7e7a8
simoninns <simon.inns@gmail.com>
2018-09-22 22:16:23 +0200

    Added validation logic to the automatic capture dialogue

--- gui-app/Linux-Application/DomesdayDuplicator/automaticcapturedialog.cpp
+++ gui-app/Linux-Application/DomesdayDuplicator/automaticcapturedialog.cpp
@@ -38,6 +38,7 @@ AutomaticCaptureDialog::AutomaticCaptureDialog(QWidget *parent) :
     ui->wholeDiscRadioButton->setChecked(true);
     ui->partialDiscRadioButton->setChecked(false);
     ui->leadInCaptureRadioButton->setChecked(false);
+    captureType = CaptureType::wholeDisc;
 
     ui->captureStatusLabel->setText("Idle");
     ui->startFrameLineEdit->setText("0");
@@ -55,6 +56,12 @@ AutomaticCaptureDialog::AutomaticCaptureDialog(QWidget *parent) :
 
     ui->startCavCapturePushButton->setEnabled(true);
     ui->startClvCapturePushButton->setEnabled(true);
+
+    // Set up validators for the entry fields
+
+    // Limit frame number entry QLineEdit fields to number only
+    ui->startFrameLineEdit->setValidator( new QIntValidator(0, 60000, this) );
+    ui->endFrameLineEdit->setValidator( new QIntValidator(0, 60000, this) );
 
     // Default state variables
     captureInProgress = false;
@@ -71,6 +78,15 @@ void AutomaticCaptureDialog::captureComplete()
     ui->partialDiscRadioButton->setEnabled(true);
     ui->leadInCaptureRadioButton->setEnabled(true);
 
+    ui->startCavCapturePushButton->setEnabled(true);
+    ui->startClvCapturePushButton->setEnabled(true);
+
+    // Call the clicked action to set up the fields correctly
+    if (ui->wholeDiscRadioButton->isChecked()) on_wholeDiscRadioButton_clicked();
+    if (ui->partialDiscRadioButton->isChecked()) on_partialDiscRadioButton_clicked();
+    if (ui->leadInCaptureRadioButton->isChecked()) on_leadInCaptureRadioButton_clicked();
+
+    // Place default values in the  fields
     ui->captureStatusLabel->setText("Idle");
     ui->startFrameLineEdit->setText("0");
     ui->endFrameLineEdit->setText("0");
@@ -79,14 +95,6 @@ void AutomaticCaptureDialog::captureComplete()
     defaultTime.setHMS(0, 0, 0, 0);
     ui->startTimeTimeEdit->setTime(defaultTime);
     ui->endTimeTimeEdit->setTime(defaultTime);
-
-    ui->startCavCapturePushButton->setEnabled(true);
-    ui->startClvCapturePushButton->setEnabled(true);
-
-    // Call the clicked action to set up the fields correctly
-    if (ui->wholeDiscRadioButton->isChecked()) on_wholeDiscRadioButton_clicked();
-    if (ui->partialDiscRadioButton->isChecked()) on_partialDiscRadioButton_clicked();
-    if (ui->leadInCaptureRadioButton->isChecked()) on_leadInCaptureRadioButton_clicked();
 }
 
 // Private methods ----------------------------------------------------------------------------------------------------
@@ -97,6 +105,7 @@ void AutomaticCaptureDialog::on_wholeDiscRadioButton_clicked()
     ui->endFrameLineEdit->setEnabled(false);
     ui->startTimeTimeEdit->setEnabled(false);
     ui->endTimeTimeEdit->setEnabled(false);
+    captureType = CaptureType::wholeDisc;
 }
 
 void AutomaticCaptureDialog::on_partialDiscRadioButton_clicked()
@@ -105,6 +114,7 @@ void AutomaticCaptureDialog::on_partialDiscRadioButton_clicked()
     ui->endFrameLineEdit->setEnabled(true);
     ui->startTimeTimeEdit->setEnabled(true);
     ui->endTimeTimeEdit->setEnabled(true);
+    captureType = CaptureType::partialDisc;
 }
 
 void AutomaticCaptureDialog::on_leadInCaptureRadioButton_clicked()
@@ -113,11 +123,31 @@ void AutomaticCaptureDialog::on_leadInCaptureRadioButton_clicked()
     ui->endFrameLineEdit->setEnabled(true);
     ui->startTimeTimeEdit->setEnabled(false);
     ui->endTimeTimeEdit->setEnabled(true);
+    captureType = CaptureType::leadInCapture;
 }
 
 void AutomaticCaptureDialog::on_startCavCapturePushButton_clicked()
 {
     if (!captureInProgress) {
+        qint32 startFrame = ui->startFrameLineEdit->text().toInt();
+        qint32 endFrame = ui->endFrameLineEdit->text().toInt();
+
+        // Validate fields according to capture type
+        if ((captureType == CaptureType::partialDisc) &&
+                ((startFrame == endFrame) || (startFrame > endFrame))) {
+            // Show an error
+            QMessageBox messageBox;
+            messageBox.warning(this, "Warning", tr("The end frame must exceed the start frame!"));
+            messageBox.setFixedSize(500, 200);
+            return;
+        } else if ((captureType == CaptureType::leadInCapture) && (endFrame == 0)) {
+            // Show an error
+            QMessageBox messageBox;
+            messageBox.warning(this, "Warning", tr("The end frame be at least 1"));
+            messageBox.setFixedSize(500, 200);
+            return;
+        }
+
         ui->wholeDiscRadioButton->setEnabled(false);
         ui->partialDiscRadioButton->setEnabled(false);
         ui->leadInCaptureRadioButton->setEnabled(false);
@@ -129,7 +159,8 @@ void AutomaticCaptureDialog::on_startCavCapturePushButton_clicked()
 
         ui->startClvCapturePushButton->setEnabled(false);
         ui->startCavCapturePushButton->setText("Abort");
-        emit startAutomaticCapture(0, 0, 0, 0);
+
+        emit startAutomaticCapture(captureType, startFrame, endFrame, DiscType::CAV);
     } else {
         // Abort
         emit stopAutomaticCapture();
@@ -139,6 +170,37 @@ void AutomaticCaptureDialog::on_startCavCapturePushButton_clicked()
 void AutomaticCaptureDialog::on_startClvCapturePushButton_clicked()
 {
     if (!captureInProgress) {
+        // Create a formatted 7-character string of the time-code
+        QString startTimeCodeString;
+        startTimeCodeString.sprintf("%01d%02d%02d00", ui->startTimeTimeEdit->time().hour(),
+                         ui->startTimeTimeEdit->time().minute(),
+        ui->startTimeTimeEdit->time().second());
+
+        QString endTimeCodeString;
+        endTimeCodeString.sprintf("%01d%02d%02d00", ui->endTimeTimeEdit->time().hour(),
+                         ui->endTimeTimeEdit->time().minute(),
+        ui->endTimeTimeEdit->time().second());
+
+        // Convert start and end time codes to integers
+        qint32 startTimeCode = startTimeCodeString.toInt();
+        qint32 endTimeCode = endTimeCodeString.toInt();
+
+        // Validate fields according to capture type
+        if ((captureType == CaptureType::partialDisc) &&
+                ((startTimeCode == endTimeCode) || (startTimeCode > endTimeCode))) {
+            // Show an error
+            QMessageBox messageBox;
+            messageBox.warning(this, "Warning", tr("The end time must exceed the start time!"));
+            messageBox.setFixedSize(500, 200);
+            return;
+        } else if ((captureType == CaptureType::leadInCapture) && (endTimeCode == 0)) {
+            // Show an error
+            QMessageBox messageBox;
+            messageBox.warning(this, "Warning", tr("The end time be at least 1 second"));
+            messageBox.setFixedSize(500, 200);
+            return;
+        }
+
         ui->wholeDiscRadioButton->setEnabled(false);
         ui->partialDiscRadioButton->setEnabled(false);
         ui->leadInCaptureRadioButton->setEnabled(false);
@@ -150,7 +212,8 @@ void AutomaticCaptureDialog::on_startClvCapturePushButton_clicked()
 
         ui->startCavCapturePushButton->setEnabled(false);
         ui->startClvCapturePushButton->setText("Abort");
-        emit startAutomaticCapture(0, 0, 0, 0);
+
+        emit startAutomaticCapture(captureType, startTimeCode, endTimeCode, DiscType::CLV);
     } else {
         emit stopAutomaticCapture();
     }
