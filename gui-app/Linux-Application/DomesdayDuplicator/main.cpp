52f105c28138ba0d5e76dbc70c9ee6137a6a79cd
simoninns <simon.inns@gmail.com>
2018-11-25 20:02:29 +0100

    Fix for issue #71 (custom debug handler)

--- gui-app/Linux-Application/DomesdayDuplicator/main.cpp
+++ gui-app/Linux-Application/DomesdayDuplicator/main.cpp
@@ -27,10 +27,85 @@
 
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDebug>
+#include <QtGlobal>
+#include <QCommandLineParser>
+
+// Global for debug output
+static bool showDebug = false;
+
+// Qt debug message handler
+void debugOutputHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    // Use:
+    // context.file - to show the filename
+    // context.line - to show the line number
+    // context.function - to show the function name
+
+    QByteArray localMsg = msg.toLocal8Bit();
+    switch (type) {
+    case QtDebugMsg: // These are debug messages meant for developers
+        if (showDebug) {
+            // If the code was compiled as 'release' the context.file will be NULL
+            if (context.file != nullptr) fprintf(stderr, "Debug: [%s:%d] %s\n", context.file, context.line, localMsg.constData());
+            else fprintf(stderr, "Debug: %s\n", localMsg.constData());
+        }
+        break;
+    case QtInfoMsg: // These are information messages meant for end-users
+        if (context.file != nullptr) fprintf(stderr, "Info: [%s:%d] %s\n", context.file, context.line, localMsg.constData());
+        else fprintf(stderr, "Info: %s\n", localMsg.constData());
+        break;
+    case QtWarningMsg:
+        if (context.file != nullptr) fprintf(stderr, "Warning: [%s:%d] %s\n", context.file, context.line, localMsg.constData());
+        else fprintf(stderr, "Warning: %s\n", localMsg.constData());
+        break;
+    case QtCriticalMsg:
+        if (context.file != nullptr) fprintf(stderr, "Critical: [%s:%d] %s\n", context.file, context.line, localMsg.constData());
+        else fprintf(stderr, "Critical: %s\n", localMsg.constData());
+        break;
+    case QtFatalMsg:
+        if (context.file != nullptr) fprintf(stderr, "Fatal: [%s:%d] %s\n", context.file, context.line, localMsg.constData());
+        else fprintf(stderr, "Fatal: %s\n", localMsg.constData());
+        abort();
+    }
+}
 
 int main(int argc, char *argv[])
 {
+    // Install the local debug message handler
+    qInstallMessageHandler(debugOutputHandler);
+
     QApplication a(argc, argv);
+
+    // Set application name and version
+    QCoreApplication::setApplicationName("DomesdayDuplicator");
+    QCoreApplication::setApplicationVersion("2.0");
+    QCoreApplication::setOrganizationDomain("domesday86.com");
+
+    // Set up the command line parser
+    QCommandLineParser parser;
+    parser.setApplicationDescription(
+        "DomesdayDuplicator - LaserDisc RF sampler\n"
+        "\n"
+        "(c)2018 Simon Inns\n"
+        "GPLv3 Open-Source - github: https://github.com/simoninns/DomesdayDuplicator");
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    // Option to show debug (-d)
+    QCommandLineOption showDebugOption(QStringList() << "d" << "debug",
+                                       QCoreApplication::translate("main", "Show debug"));
+    parser.addOption(showDebugOption);
+
+    // Process the command line arguments given by the user
+    parser.process(a);
+
+    // Get the configured settings from the parser
+    bool isDebugOn = parser.isSet(showDebugOption);
+
+    // Process the command line options
+    if (isDebugOn) showDebug = true;
+
     MainWindow w;
     w.show();
 
