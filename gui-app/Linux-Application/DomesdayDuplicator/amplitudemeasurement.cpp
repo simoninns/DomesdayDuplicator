4019113601564dba812526793c679ce26a79bbc8
Adam Sampson <ats@offog.org>
2022-12-18 21:07:08 +0000

    Add copyright notices to new files.

--- gui-app/Linux-Application/DomesdayDuplicator/amplitudemeasurement.cpp
+++ gui-app/Linux-Application/DomesdayDuplicator/amplitudemeasurement.cpp
@@ -1,3 +1,30 @@
+/************************************************************************
+
+    amplitudemeasurement.cpp
+
+    Capture application for the Domesday Duplicator
+    DomesdayDuplicator - LaserDisc RF sampler
+    Copyright (C) 2022 Matt Perry
+
+    This file is part of Domesday Duplicator.
+
+    Domesday Duplicator is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Email: simon.inns@gmail.com
+
+************************************************************************/
+
 #include <QtWidgets>
 #include <QtMultimedia>
 #include <QAudioBuffer>
