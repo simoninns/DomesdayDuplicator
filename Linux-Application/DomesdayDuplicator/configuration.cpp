a456a95b6b3fb48158be743935ed20558268bd78
Adam Sampson <ats@offog.org>
2022-12-18 22:00:25 +0000

    Rework how amplitude buffers are captured.
    Previously, when an amplitude display was enabled, the UsbCapture thread
    converted every disk buffer into 16-bit form and copied it to a
    temporary buffer, which was very expensive, and meant it had to know
    whether the display was enabled.
    
    Now it's done on request: the static function that returns a disk buffer
    gives back the most recently-captured one, and sets a flag saying that a
    new one should be captured. So overall the UsbCapture thread will only
    convert buffers at the rate the display is updating.
    
    The expensive part of the conversion now happens in the main thread, so
    it should interfere less with the capture thread. This also considerably
    simplifies the conversion code -- there's no need to use QtMultimedia,
    which makes it compatible with Qt 5 again.

--- Linux-Application/DomesdayDuplicator/configuration.cpp
+++ Linux-Application/DomesdayDuplicator/configuration.cpp
@@ -53,10 +53,6 @@ Configuration::Configuration(QObject *parent) : QObject(parent)
     }
 }
 
-// Init variables for amplitude configuration
-bool processTextAmplitude = false;
-bool processGraphAmplitude = false;
-
 void Configuration::writeConfiguration(void)
 {
     // Write the valid configuration flag
@@ -327,7 +323,6 @@ void Configuration::setAmplitudeEnabled(bool amplitudeEnabled)
 
 bool Configuration::getAmplitudeEnabled(void)
 {
-    processTextAmplitude = settings.ui.amplitudeEnabled;
     return settings.ui.amplitudeEnabled;
 }
 
@@ -338,11 +333,6 @@ void Configuration::setGraphType(GraphType graphType)
 
 Configuration::GraphType Configuration::getGraphType(void)
 {
-    if (settings.ui.graphType == noGraph) {
-        processGraphAmplitude = false;
-    } else {
-        processGraphAmplitude = true;
-    }
     return settings.ui.graphType;
 }
 
@@ -395,13 +385,4 @@ void Configuration::setConfigurationDialogGeometry(QByteArray configurationDialo
 QByteArray Configuration::getConfigurationDialogGeometry(void)
 {
     return settings.windows.configurationDialogGeometry;
-}
-
-bool Configuration::getProcessAmplitude(void)
-{
-    if (processGraphAmplitude || processTextAmplitude) {
-        return true;
-    } else {
-        return false;
-    }
 }
